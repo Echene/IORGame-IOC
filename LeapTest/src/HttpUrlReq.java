1c3c8801318b4781f51cc9f08e49af028349afd1
Mkenini Zaatout <mkeninismael2@gmail.com>
2017-04-03 09:02:59 +0200

    tutu

--- LeapTest/src/HttpUrlReq.java
+++ LeapTest/src/HttpUrlReq.java
@@ -8,6 +8,8 @@ import java.net.URLEncoder;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import sun.applet.Main;
+
 public class HttpUrlReq {
 	
 	public HttpUrlReq(){}
@@ -18,21 +20,31 @@ public class HttpUrlReq {
 		String message = URLEncoder.encode(instruction, "UTF-8");
 		URL objUrl = new URL("http://iotserver.univ-brest.fr/robot.php");
 		HttpURLConnection connexion = (HttpURLConnection) objUrl.openConnection();
-		
-		connexion.setRequestMethod("POST");
+		 // set connection output to true
+		connexion.setDoOutput(true);
+		connexion.setRequestMethod("GET");
 		OutputStreamWriter writer = new OutputStreamWriter(connexion.getOutputStream());
 		writer.write(message);
-		writer.flush();
-		
+		writer.close();
 		if (connexion.getResponseCode() == HttpURLConnection.HTTP_OK) {
 	        // OK
 			logger.log(Level.SEVERE,"Envoie reussi : "+connexion.getResponseCode());
-	        // otherwise, if any other status code is returned, or no status
+			// connexion.disconnect();
+			// otherwise, if any other status code is returned, or no status
 	        // code is returned, do stuff in the else block
 	    } else {
 	        // Server returned HTTP error code.
 	    	logger.log(Level.SEVERE,"Echec : "+connexion.getResponseCode());
 	    }
 	}
-
+	public static void main(String[] args) {
+		HttpUrlReq obj = new HttpUrlReq();
+		try {
+			// Allumer le robot hand
+			obj.envoi("?x1=1&x2=0&x3=0");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
