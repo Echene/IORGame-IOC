3db21b55ff601016be5d7a123e44ff173ede20a2
Harshil SETHI <harshil@hotmail.fr>
2017-04-03 16:01:46 +0200

    Ajout des détections mouvemens Palm Left et Right

--- LeapTest/src/SampleListener.java
+++ LeapTest/src/SampleListener.java
@@ -1,8 +1,11 @@
 import java.util.ResourceBundle.Control;
 
 import com.leapmotion.leap.Controller;
+import com.leapmotion.leap.Finger;
 import com.leapmotion.leap.Frame;
 import com.leapmotion.leap.Listener;
+import com.leapmotion.leap.Pointable;
+import com.leapmotion.leap.Vector;
 import com.leapmotion.leap.Hand;
 import com.leapmotion.leap.HandList;
 
@@ -19,14 +22,26 @@ public class SampleListener extends Listener {
 
     public void onFrame(Controller controller) {
         Frame frame = controller.frame();
-        //HandList hands = frame.hands();
+        HandList hands = frame.hands();
+        Hand hand = hands.leftmost();
         
-        //Hand leftmost = hands.leftmost();
+        Pointable pointable = frame.pointables().frontmost();
+        Vector direction = pointable.direction();
+        float length = pointable.length();
+        float width = pointable.width();
+        Vector stabilizedPosition = pointable.stabilizedTipPosition();
+        Vector position = pointable.tipPosition();
+        Vector speed = pointable.tipVelocity();
+        float touchDistance = pointable.touchDistance();
+        Pointable.Zone zone = pointable.touchZone();
 
         System.out.println("Frame id: " + frame.id()
-                       + ", timestamp: " + frame.timestamp()
-                       + ", hands: " + frame.hands().count()
-                       + ", fingers: " + frame.fingers().count());
+                       + ", Timestamp: " + frame.timestamp()
+                       + ", Hands: " + frame.hands().count()
+                       + ", Fingers: " + frame.fingers().count()
+                       + ", Palm: " + pointable.isExtended()
+                       + ", Left: " + hand.isLeft()
+                       + ", Right: " + hand.isRight());
                        //+ ", left: " + leftmost);
     }
 
