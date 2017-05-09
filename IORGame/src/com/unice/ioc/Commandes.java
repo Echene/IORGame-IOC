e71d7d911000f5bb8ce6a38c4ed51ea8f1881a93
Echene <emma.chene@gmail.com>
2017-05-09 11:30:15 +0200

    Ajout du code pour le LeapMotion

--- IORGame/src/com/unice/ioc/Commandes.java
+++ IORGame/src/com/unice/ioc/Commandes.java
@@ -1,17 +1,13 @@
 package com.unice.ioc;
 
 
-import java.net.HttpURLConnection;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.InputStreamReader;
-import java.net.URL;
+
 
 public class Commandes implements KeyListener {
 
-    private final String USER_AGENT = "Mozilla/5.0";
+
 
     public void keyTyped(KeyEvent e) {
 
@@ -19,73 +15,56 @@ public class Commandes implements KeyListener {
 
     public void keyPressed(KeyEvent e) {
 
-        String ordre = "";
-        String url = "";
+        String order = "";
+        String URL = "";
+        Boolean OK = true;
+
+        HttpConnection httpConnection = new HttpConnection();
 
         try{
 
             if (e.getKeyCode() == KeyEvent.VK_UP) {
-                url = "http://iotserver.univ-brest.fr/robot.php?x1=0&x2=0&x3=1";
-                ordre = "UP";
+                URL = "http://iotserver.univ-brest.fr/robot.php?x1=0&x2=0&x3=1";
+                order = "Avancer - UP";
 
             } else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
-                url = "http://iotserver.univ-brest.fr/robot.php?x1=1&x2=0&x3=1";
-                ordre = "DOWN";
+                URL = "http://iotserver.univ-brest.fr/robot.php?x1=1&x2=0&x3=1";
+                order = "Reculer - DOWN";
 
             } else if (e.getKeyCode() == KeyEvent.VK_LEFT) {
-                url = "http://iotserver.univ-brest.fr/robot.php?x1=0&x2=1&x3=0";
-                ordre = "LEFT";
+                URL = "http://iotserver.univ-brest.fr/robot.php?x1=0&x2=1&x3=0";
+                order = "Gauche - LEFT";
 
             } else if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
-                url = "http://iotserver.univ-brest.fr/robot.php?x1=1&x2=1&x3=0";
-                ordre = "RIGHT";
+                URL = "http://iotserver.univ-brest.fr/robot.php?x1=1&x2=1&x3=0";
+                order = "Droite - RIGHT";
 
             } else if (e.getKeyCode() == KeyEvent.VK_SPACE) {
-                url = "http://iotserver.univ-brest.fr/robot.php?x1=0&x2=1&x3=1";
-                ordre = "SPACE";
+                URL = "http://iotserver.univ-brest.fr/robot.php?x1=0&x2=1&x3=1";
+                order = "Monter - SPACE";
 
             } else if (e.getKeyCode() == KeyEvent.VK_CONTROL) {
-                url = "http://iotserver.univ-brest.fr/robot.php?x1=1&x2=1&x3=1";
-                ordre = "CONTROL";
+                URL = "http://iotserver.univ-brest.fr/robot.php?x1=1&x2=1&x3=1";
+                order = "Descendre - CONTROL";
+
+            } else if (e.getKeyCode() == KeyEvent.VK_Z) {
+                URL = "http://iotserver.univ-brest.fr/robot.php?x1=1&x2=0&x3=0";
+                order = "Ouvrir - Z";
+
+            } else if (e.getKeyCode() == KeyEvent.VK_S) {
+                URL = "http://iotserver.univ-brest.fr/robot.php?x1=0&x2=0&x3=0";
+                order = "Fermer - S";
 
             } else {
-
+                OK = false;
             }
 
-
-
-            URL obj = new URL(url);
-            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-
-            //add request header
-            con.setRequestMethod("POST");
-            con.setRequestProperty("User-Agent", USER_AGENT);
-            con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
-
-
-            // Send post request
-            con.setDoOutput(true);
-            DataOutputStream wr = new DataOutputStream(con.getOutputStream());
-            wr.flush();
-            wr.close();
-
-            int responseCode = con.getResponseCode();
-            System.out.println("\nSending 'POST' request to URL : " + url);
-            System.out.println("Post parameters : " +  ordre);
-            System.out.println("Response Code : " + responseCode);
-
-            BufferedReader in = new BufferedReader(
-                    new InputStreamReader(con.getInputStream()));
-            String inputLine;
-            StringBuffer response = new StringBuffer();
-
-            while ((inputLine = in.readLine()) != null) {
-                response.append(inputLine);
+            if (OK){
+                httpConnection.sendOrder(URL, order);
+            } else {
+                System.out.println("Commande non reconnue");
             }
-            in.close();
 
-            //print result
-            System.out.println(response.toString());
 
         } catch (Exception ex){
             System.out.print(ex);
@@ -95,5 +74,63 @@ public class Commandes implements KeyListener {
 
     public void keyReleased(KeyEvent e) {
 
+    }
+
+    public void SendCommande(String commande) {
+
+        String order = "";
+        String URL = "";
+        Boolean OK = true;
+
+        HttpConnection httpConnection = new HttpConnection();
+
+        try{
+
+            if (commande == "AVANCER") {
+                URL = "http://iotserver.univ-brest.fr/robot.php?x1=0&x2=0&x3=1";
+                order = "Avancer - UP";
+
+            } else if (commande == "RECULER") {
+                URL = "http://iotserver.univ-brest.fr/robot.php?x1=1&x2=0&x3=1";
+                order = "Reculer - DOWN";
+
+            } else if (commande == "GAUCHE") {
+                URL = "http://iotserver.univ-brest.fr/robot.php?x1=0&x2=1&x3=0";
+                order = "Gauche - LEFT";
+
+            } else if (commande == "DROITE") {
+                URL = "http://iotserver.univ-brest.fr/robot.php?x1=1&x2=1&x3=0";
+                order = "Droite - RIGHT";
+
+            } else if (commande == "MONTER") {
+                URL = "http://iotserver.univ-brest.fr/robot.php?x1=0&x2=1&x3=1";
+                order = "Monter - SPACE";
+
+            } else if (commande == "DESCENDRE") {
+                URL = "http://iotserver.univ-brest.fr/robot.php?x1=1&x2=1&x3=1";
+                order = "Descendre - CONTROL";
+
+            } else if (commande == "OUVRIR") {
+                URL = "http://iotserver.univ-brest.fr/robot.php?x1=1&x2=0&x3=0";
+                order = "Ouvrir - Z";
+
+            } else if (commande == "FERMER") {
+                URL = "http://iotserver.univ-brest.fr/robot.php?x1=0&x2=0&x3=0";
+                order = "Fermer - S";
+
+            } else {
+                OK = false;
+            }
+
+            if (OK){
+                httpConnection.sendOrder(URL, order);
+            } else {
+                System.out.println("Commande non reconnue");
+            }
+
+
+        } catch (Exception ex){
+            System.out.print(ex);
+        }
     }
 }
--- IORGame/src/com/unice/ioc/HttpConnection.java
+++ IORGame/src/com/unice/ioc/HttpConnection.java
@@ -1,33 +1,53 @@
 package com.unice.ioc;
 
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
 import java.net.URL;
 
 
 public class HttpConnection {
 
-    public void sendPost(String fichier, String commande) throws Exception {
-        String urlParameters = commande;
-        URL url = new URL(fichier);
-        java.net.URLConnection conn = url.openConnection();
+    private final String USER_AGENT = "Mozilla/5.0";
 
-        conn.setDoOutput(true);
+    public void sendOrder(String URL, String order) throws Exception {
 
-        OutputStreamWriter writer = new OutputStreamWriter(conn.getOutputStream());
+        URL obj = new URL(URL);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
 
-        writer.write(urlParameters);
-        writer.flush();
+        //Add request header
+        con.setRequestMethod("POST");
+        con.setRequestProperty("User-Agent", USER_AGENT);
+        con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
 
-        String line;
-        BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
 
-        while ((line = reader.readLine()) != null) {
-            System.out.println(line);
+        // Send request
+        con.setDoOutput(true);
+        DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+        wr.flush();
+        wr.close();
+
+        int responseCode = con.getResponseCode();
+        System.out.println("\nSending 'POST' request to URL : " + URL);
+        System.out.println("Post parameters : " +  order);
+        System.out.println("Response Code : " + responseCode);
+
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+            System.out.println(inputLine);
         }
-        writer.close();
-        reader.close();
+        in.close();
+
+        //Print result
+        System.out.println(response.toString());
+
     }
 
 }
